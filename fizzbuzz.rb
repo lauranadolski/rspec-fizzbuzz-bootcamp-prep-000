0fed6b90a8e8eac278ab4c17b6f954305c85e63b
Laura Nadolski <laura.nadolski@gmail.com>
2018-03-28 03:13:56 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(int)
-  if int % 3 == 0 && int > 15
+  if int % 3 == 0 && int < 15
   "Fizz"
-  elsif int % 5 == 0 && int > 15
+  elsif int % 5 == 0 && int < 15
   "Buzz"
   elsif int % 15 == 0
   "FizzBuzz"
